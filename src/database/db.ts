160f28115b3b18f9d094a93e3c4ccf47c1bde267
Mukunzijames <mukunzindahiro@gmail.com>
2024-03-31 07:58:16 +0200

    test(circile ci automation ):setting up code climate
    i was setting up circle ci and code climate
    
    [#18730013]

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,25 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Sequelize } from 'sequelize';
 import getDatabaseConfig from '../config/config';
 import Models from './models';
 
-
+const { username, database, password, host } = getDatabaseConfig();
 const sequelize = new Sequelize(database, username, password, {
-  host:host,
+  host: host,
   dialect: 'postgres',
 });
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.'); // eslint-disable-line no-console
+    console.log('Connection has been established successfully.');
   })
-  .catch((err: Error) => {
-    console.error('Unable to connect to the database:', err); // eslint-disable-line no-console
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1); // Exit the process on connection error
   });
+
 const models = Models(sequelize);
 
-
+Object.values(models).forEach((model) => {
+  if (model.associate) {
+    model.associate(models);
   }
 });
 
