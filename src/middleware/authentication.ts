f024f4101e1bdfbebd845f55b3c03da64fc3cc58
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-04-12 10:05:31 +0200

    -amend

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -13,7 +13,7 @@ export const isAuthenticated = (
     { session: false },
     (err: Error, user: UserAttributes) => {
       if (!user) {
-        return sendResponse(res, 401, null, 'Unauthorized');
+        return sendResponse(res, 403, null, 'Please login...');
       }
       const currUser = {
         id: user.id,
--- test/updatePassword.test.ts
+++ test/updatePassword.test.ts
@@ -2,84 +2,72 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/app';
 import { describe, it } from 'mocha';
-import sinon from 'sinon';
-import Database from '../src/database';
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 chai.use(chaiHttp);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const userPassword = {
+  oldPassword: '1111@aa',
+  newPassword: 'Password@3',
+  confirmPassword: 'Password@3',
+};
 
 describe('POST, /api/users/change-password', () => {
-  it('it should return status code of 200', async () => {
-    const userPassword = {
-      oldPassword: '1111@aa',
-      newPassword: 'Password@3',
-      confirmPassword: 'Password@3',
-    };
+  var token: string = '';
 
-    const user = await chai.request(app).post('/api/users/login').send({
+  before(done => {
+    const user = {
       email: 'test1@example.com',
       password: userPassword.oldPassword,
-    });
-
-    const Token: string = user.body.data;
-
+    };
+    chai
+      .request(app)
+      .post('/api/users/login')
+      .set('Authorization', `Bearer ${token}`)
+      .send(user)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('data');
+        token = res.body.data;
+        done();
+      });
+  });
+  it('it should return status code of 200 for successfully change password', done => {
     chai
       .request(app)
       .post('/api/users/change-password')
-      .set('Authorization', `Bearer ${Token}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(userPassword)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body).to.have.property('message');
         expect(res.body.message).to.equal('Password changed successfully');
+        done();
       });
   });
-  it('when old password is not the same as the one stored in database', async () => {
-    const userPassword = {
-      oldPassword: '1111aa',
-      newPassword: 'Password@31',
-      confirmPassword: 'Password@31',
-    };
-
-    const user = await chai.request(app).post('/api/users/login').send({
-      email: 'test1@example.com',
-      password: '1111@aa',
-    });
-
-    const Token: string = user.body.data;
-
+  it('when old password is not the same as the one stored in database', done => {
     chai
       .request(app)
       .post('/api/users/change-password')
-      .set('Authorization', `Bearer ${Token}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(userPassword)
       .end((err, res) => {
         expect(res.body.message).to.equal('Invalid credentials');
+        done();
       });
   });
-  it('in case of invalid or no token', async () => {
-    const userPassword = {
-      oldPassword: '1111@aa',
-      newPassword: 'Password@3',
-      confirmPassword: 'Password@3',
-    };
-    const user = await chai.request(app).post('/api/users/login').send({
-      email: 'test1@example.com',
-      password: '1111@aa',
-    });
-
-    const Token: string = user.body.data;
-
+  it('in case of invalid or no token', done => {
+    token = '';
     chai
       .request(app)
       .post('/api/users/change-password')
-      .set('Authorization', `Bearer ${Token}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(userPassword)
       .end((err, res) => {
         expect(res.body).to.have.property('message');
-        expect(res.body.message).to.equal('Unauthorized');
+        expect(res.body.message).to.equal('Please login...');
+        done();
       });
   });
 });
