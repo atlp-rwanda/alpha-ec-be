1c5835a8aef4c69d0f863867dfa24b96286ce2e3
Musabe10 <155322569+musabehonore@users.noreply.github.com>
2024-07-01 20:49:00 +0200

    fix(cart&wishlist): cart & wishlist responses (#72)
    fixed cart and wishlist responses to look the same accross all the backend requests

--- src/controllers/cartsController.ts
+++ src/controllers/cartsController.ts
@@ -25,7 +25,9 @@ export const addItemToCart = async (req: Request, res: Response) => {
     if (validProduct.quantity < product.quantity) {
       return sendResponse<null>(res, 406, null, 'Quantity Exceeds Stock');
     }
-    const cartExist = await Database.Cart.findOne({ where: { userId: id } });
+    const cartExist = await Database.Cart.findOne({
+      where: { userId: id },
+    });
     if (!cartExist) {
       const cart = Database.Cart.build({
         userId: id,
@@ -33,7 +35,12 @@ export const addItemToCart = async (req: Request, res: Response) => {
         totalprice: validProduct.price * product.quantity,
       });
       await cart.save();
-      return sendResponse(res, 201, cart, 'Product added to cart');
+      const returnCart = {
+        id: cart.id,
+        produtcs: await formatCartItems([product]),
+        totalprice: cart.totalprice,
+      };
+      return sendResponse(res, 201, returnCart, 'Product added to cart');
     }
     const findProduct = cartExist.products.find(
       p => p.productId === product.productId
@@ -49,11 +56,17 @@ export const addItemToCart = async (req: Request, res: Response) => {
 
     const updatedProducts = [...cartExist.products, product];
     const updatedTotalPrice = await calculateTotal(updatedProducts);
-    const updatedCart = await Database.Cart.update(
+    await Database.Cart.update(
       { products: updatedProducts, totalprice: updatedTotalPrice },
       { where: { id: cartExist.id }, returning: true }
     );
-    return sendResponse(res, 201, updatedCart[1], 'Product is added to cart ');
+
+    const returnCart2 = {
+      id: cartExist.id,
+      produtcs: await formatCartItems(updatedProducts),
+      totalprice: updatedTotalPrice,
+    };
+    return sendResponse(res, 201, returnCart2, 'Product is added to cart ');
   } catch (err: unknown) {
     const errors = err as Error;
     return sendResponse<null>(res, 500, null, errors.message);
@@ -110,11 +123,16 @@ export const updateCart = async (req: Request, res: Response) => {
       return p;
     });
     const updatedTotalPrice = await calculateTotal(updatedProducts);
-    const updatedCart = await Database.Cart.update(
+    await Database.Cart.update(
       { products: updatedProducts, totalprice: updatedTotalPrice },
       { where: { id: cartFound.id }, returning: true }
     );
-    return sendResponse(res, 201, updatedCart[1], 'Cart updated');
+    const returnCart2 = {
+      id: cartFound.id,
+      produtcs: await formatCartItems(updatedProducts),
+      totalprice: updatedTotalPrice,
+    };
+    return sendResponse(res, 201, returnCart2, 'Cart updated');
   } catch (err: unknown) {
     const errors = err as Error;
     return sendResponse<null>(res, 500, null, errors.message);
@@ -158,11 +176,17 @@ export const deleteProductFromCart = async (req: Request, res: Response) => {
       p => p.productId !== productId
     );
     const updatedTotalPrice = await calculateTotal(updatedProducts);
-    const updatedCart = await Database.Cart.update(
+    await Database.Cart.update(
       { products: updatedProducts, totalprice: updatedTotalPrice },
       { where: { id: cartFound.id }, returning: true }
     );
-    return sendResponse(res, 200, updatedCart[1], 'Product deleted');
+    const returnCart2 = {
+      id: cartFound.id,
+      produtcs: await formatCartItems(updatedProducts),
+      totalprice: updatedTotalPrice,
+    };
+
+    return sendResponse(res, 200, returnCart2, 'Product deleted');
   } catch (err: unknown) {
     const errors = err as Error;
     return sendResponse<null>(res, 500, null, errors.message);
